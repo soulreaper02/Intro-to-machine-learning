59807d30d9313c9733147c0a5e0ad532faeaef17
Amit Kumar <slayer9191@gmail.com>
2018-08-17 14:39:21 +0530

    naive bayes code push

---
+++ naive_bayes/nb_author_id.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+
+""" 
+    Use a Naive Bayes Classifier to identify emails by their authors
+    
+    authors and labels:
+    Sara has label 0
+    Chris has label 1
+"""
+    
+import sys
+from time import time
+sys.path.append("../tools/")
+from email_preprocess import preprocess
+
+
+### features_train and features_test are the features for the training
+### and testing datasets, respectively
+### labels_train and labels_test are the corresponding item labels
+features_train, features_test, labels_train, labels_test = preprocess()
+
+
+from sklearn.naive_bayes import GaussianNB
+clf = GaussianNB()
+t0 = time()
+clf.fit(features_train, labels_train)
+print "training time:", round(time()-t0, 3), "s"
+t1 = time()
+print clf.predict(features_test)
+print "predection time:", round(time()-t1, 3), "s"
+print clf.score(features_test,labels_test)
